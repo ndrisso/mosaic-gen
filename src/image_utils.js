3fc4ecddcd058c048f315dcf4b2a93aa14574030
Nicolas Risso <ndrisso@gmail.com>
2021-04-10 21:02:58 -0300

    Add first failing test

--- src/image_utils.js
+++ src/image_utils.js
@@ -1,7 +1,38 @@
+const fs = require('fs')
+const path = require('path')
+const outputPath = 'temp'
+
+/**
+ * Squares and resizes every image in the tilesPath
+ * to 100x100 and outpus everything into a new tiles directory
+ * @param {string} tilesPath 
+ * @returns {string} Path to folder containing output
+ */
+const prepareImages = async (tilesPath) => {
+  if (fs.existsSync(tilesPath)) {
+    fs.rmdirSync(outputPath, { recursive: true })
+  }
+  fs.mkdirSync(outputPath)
+  await copyTiles(tilesPath, outputPath)
+  await squareTiles(outputPath)
+  await resizeTiles(outputPath)
+
+  return outputPath
+}
+
+const copyTiles = async (tilesPath, outputPath) => {
+  files = fs.readdirSync(tilesPath)
+  files.forEach(file => fs.copyFileSync(path.join(tilesPath, file), path.join(outputPath, file)))
+}
+
 const squareTiles = async (tilesPath) => {
-  return true;
+  return true
+}
+
+const resizeTiles = async (tilesPath) => {
+  return true
 }
 
 module.exports = {
-  resizeTiles
+  prepareImages
 }
--- test/image_utils.test.js
+++ test/image_utils.test.js
@@ -1,22 +1,42 @@
-const { resizeTiles } = require('../src/image_utils')
-const im = require('image-magick')
+const { prepareImages } = require('../src/image_utils')
+const im = require('imagemagick')
+const fs = require('fs')
+const path = require('path')
+const { resolve } = require('path')
 
-describe(('squareTiles') => {
-
+describe('prepareImages', () => {
   test('squares & resizes tiles to 100x100 pixels', async () => {
-    await resizeTiles('./test/sample_images')
-  
-    const cb = (err, metadata) => {
-      if (err) { return done(err) }
-  
-      expect(metadata.height).toBe(100)
-      expect(metadata.width).toBe(100)
-      done()
-    }
-  
-    
+    const outputPath = await prepareImages('./test/sample_images')
+    const tiles = await identifyImages(outputPath)
+    tiles.forEach(tile => {
+      expect(tile.width).toBe(100)
+      expect(tile.height).toBe(100)
+    })
   })
-  
-
 })
 
+const identifyImages = (outputPath) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(outputPath, (err, files) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      promises = files.map(file => identifyPromise(path.join(outputPath, file)))
+      resolve(Promise.all(promises))
+    })
+  })
+}
+
+const identifyPromise = (file) => {
+  return new Promise((resolve, reject) => {
+    im.identify(file, (err, features) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(features)
+      }
+    })
+  })
+}
